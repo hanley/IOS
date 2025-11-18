dfa43aece8cdf532b8e0f44505baf9ebdce3d167
hanley <hanleyloo@gmail.com>
2025-11-19 07:34:27 +0800

    Update imports to use langchain_classic module

--- ios_buddy/ios_buddy.py
+++ ios_buddy/ios_buddy.py
@@ -3,8 +3,8 @@ import requests
 import streamlit as st
 from langchain_community.llms import Ollama
 from langchain_community.vectorstores import Chroma
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain_classic.memory import ConversationBufferMemory
+from langchain_classic.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import JSONLoader, TextLoader
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
